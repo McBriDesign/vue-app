eeee8600bc1d9e991be1410e6fe9b559b55bf8d1
McBriDesign <mr.kieran.mcbride@googlemail.com>
2021-01-25 20:39:48 +0000

    added list rendering

--- js/main.js
+++ js/main.js
@@ -10,6 +10,7 @@ data: {
     link: 'http://www.sendtosay.com',
     inventory: 100,
     onSale: true,
+    details: ["Friday Night Dinner Design", "100% recyled card", "Available in all sizes", "Customisation available" ]
 }
 
 })
