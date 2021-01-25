38650904020584f170080a8072ffeb168bc41c77
Kieran McBride <mr.kieran.mcbride@googlemail.com>
2021-01-25 18:58:15 +0000

    inital commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var app = new Vue({
+
+el: '#app',
+data: {
+    product: 'Cards',
+    image: './assets/card-example.jfif',   
+    description: 'birthday ting',
+    link: 'http://www.sendtosay.com',
+    inventory: 10,
+    onSale: true,
+}
+
+})
