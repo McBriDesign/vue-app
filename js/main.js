6478fdeed2d2ec65a21a62429aaceab72252ea51
Kieran <mr.kieran.mcbride@googlemail.com>
2021-01-25 22:52:45 +0000

    added stock check funtionality to sizes on click

--- js/main.js
+++ js/main.js
@@ -8,21 +8,24 @@ data: {
     image: './assets/card-example.jfif',   
     description: 'birthday ting',
     link: 'http://www.sendtosay.com',
-    inventory: 100,
+    inventory: -1,
     onSale: true,
     details: ["Friday Night Dinner Design", "100% recyled card", "Available in all sizes", "Customisation available" ],
     variants: [
         {
             variantID: 0001,
             variantSize: "Small",
+            variantInventory: 0,
         },
         {
             variantID: 0002,
             variantSize: "Medium",
+            variantInventory: 6,
         },
         {
             variantID: 0003,
             variantSize: "Large",
+            variantInventory: 100,
         }
     ],
     cart: 0
@@ -31,6 +34,10 @@ methods: {
     addToCart: function() {
         this.cart += 1
         
+    },
+
+    updateProduct: function (variantInventory) {
+        this.inventory = variantInventory
     }
 }
 
