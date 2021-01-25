73ba7b929b81bbcbd56c02fec9a5157d39238568
McBriDesign <mr.kieran.mcbride@googlemail.com>
2021-01-25 21:01:52 +0000

    added add to cart function

--- js/main.js
+++ js/main.js
@@ -10,7 +10,28 @@ data: {
     link: 'http://www.sendtosay.com',
     inventory: 100,
     onSale: true,
-    details: ["Friday Night Dinner Design", "100% recyled card", "Available in all sizes", "Customisation available" ]
+    details: ["Friday Night Dinner Design", "100% recyled card", "Available in all sizes", "Customisation available" ],
+    variants: [
+        {
+            variantID: 0001,
+            variantSize: "Small",
+        },
+        {
+            variantID: 0002,
+            variantSize: "Medium",
+        },
+        {
+            variantID: 0003,
+            variantSize: "Large",
+        }
+    ],
+    cart: 0,
+    methods: {
+        addToCart function () {
+            this.cart += 1
+            
+        }
+    }
 }
 
 })
